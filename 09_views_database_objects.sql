767b88ebc87ec1cf09000e3b68fe4cd97d84d082
jaroslaw-kurzeja <jarek.kurzeja@gmail.com>
2026-09-07 08:07:16 +0200

    Update 09_views_database_objects.sql

---
+++ 09_views_database_objects.sql
@@ -0,0 +1,11 @@
+# Stage 9 — VIEW and Database Objects
+
+# 9.1 Find an existing VIEW in the Sakila database and analyze what it does.
+
+# 9.2 Create a VIEW showing each customer’s country and their total payments.
+
+# 9.3 Verify that the VIEW returns correct results.
+
+# 9.4 Modify the VIEW to include additional useful customer information.
+
+# 9.5 Analyze an existing trigger, stored procedure, and function in the Sakila database.
